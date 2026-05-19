73df8e663f5c90f3baf41e4a04dc2d7dda6fa9dd
Poglet44 <ninjaman1@hotmail.co.uk>
2026-05-19 19:39:25 +0100

    Updated world files, multiple world launches available

--- sim_tb3_velodyne/launch/world_ideal_mal.launch.py
+++ sim_tb3_velodyne/launch/world_ideal_mal.launch.py
@@ -22,41 +22,43 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import EnvironmentVariable, LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable
 
 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('sim_tb3_velodyne'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_tb3_gazebo = get_package_share_directory('turtlebot3_gazebo')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='-2.39')
-    y_pose = LaunchConfiguration('y_pose', default='1.1')
+    x_pose = LaunchConfiguration('x_pose', default='5.3')
+    y_pose = LaunchConfiguration('y_pose', default='4.0')
 
     world = os.path.join(
         get_package_share_directory('sim_tb3_velodyne'),
         'worlds',
-        'turtlebot3_house.world'
+        'ideal_world.world'
     )
-
+    
+    pkg_share = get_package_share_directory('sim_tb3_velodyne')
+    models_path = os.path.join(pkg_share, 'models')
+    world_models_path = os.path.join(pkg_share, 'models', 'world_models')
+    
     gazebo_model_path = SetEnvironmentVariable(
-        name='GAZEBO_MODEL_PATH',
-        value=[
-            os.path.join(pkg_tb3_gazebo, 'models'),
-            ':',
-            EnvironmentVariable('GAZEBO_MODEL_PATH', default_value='')
-        ]
+    name='GAZEBO_MODEL_PATH',
+    value=[
+        models_path,
+        ':',
+        world_models_path,
+        ':',
+        EnvironmentVariable('GAZEBO_MODEL_PATH', default_value='')
+    ]
     )
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
         ),
-        launch_arguments={
-            'world': world,
-            'extra_gazebo_args': '-s libgazebo_ros_init.so -s libgazebo_ros_factory.so',
-        }.items()
+        launch_arguments={'world': world}.items()
     )
 
     gzclient_cmd = IncludeLaunchDescription(
